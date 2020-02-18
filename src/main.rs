d0c94656a693e647a94eca9193879f5eff595621
dfirence <carlos.diaz.pro@gmail.com>
2020-02-17 22:59:17 -0500

    Reparse

--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>>
     let _sample = "/home/archir/Documents/my_code/rust/pe-compass/pe-samples/sqlite3x86.dll";
 
     let _pe = PeParser::new(_sample);
-    let _dosheader      = _pe.get_dosheader();
-    let _fileheader     = _pe.get_peheader(_dosheader.e_lfanew); 
+    let _dosheader      = _pe.get_dosheader(); 
     let _nt_headers     = _pe.get_image_nt_headers32(_dosheader.e_lfanew);
     let _pe_data_dirs   = _pe.get_data_directories(&_nt_headers.OptionalHeader.DataDirectory);
 
     println!("\n\nDOS   HEADER: \n\n{:#?}", _dosheader);
-    println!("\n\nFILE  HEADER: \n\n{:#?}", _fileheader);
-    println!("\n\nOPT   HEADER: \n\n{:#?}", _nt_headers.OptionalHeader);
+    println!("\n\nNT    HEADER: \n\n{:#?}", _nt_headers);
     println!("\n\nPE DATA DIRS: \n\n{:#?}", _pe_data_dirs);
 
     Ok(())
